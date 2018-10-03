b9d6ea15b159fd8edc83c30f78c6adee7cae5e62
Volodymyrx <volodiax@mail.ru>
2018-10-03 12:01:01 +0300

    1.1. Создание Hello World [#141] version 2

--- src/ru/job4j/calculate/Calculate.java
+++ src/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,19 @@
 package ru.job4j.calculate;
+/**
+*Calculate
+*
+*@author Volodymyr Martynenko (VolodymyrV.Martynenko@gmail.com)
+*project job4j lesson 1.1 
+*version 0.2
+*/
 
 public class Calculate
 {
+	/**
+	*main
+	*@param -no
+	*print on console text 'Hello world'
+	*/
 	public static void main(String[] args)
 	{
 		System.out.println("Hello world");
