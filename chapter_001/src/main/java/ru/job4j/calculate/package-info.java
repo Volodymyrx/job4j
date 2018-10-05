e9157a49f3671ee0af2ecc3f73020440c849bfc7
Volodymyrx <volodiax@mail.ru>
2018-10-05 21:44:24 +0300

    1.3. JavaDoc. Code convention [#4087]plus

---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,9 @@
+/**
+*Package for calculate task.
+*
+*@author Volodymyr Martynenko (VolodymyrV.Martynenko@gmail.com)
+*project job4j lesson 1.3 
+*@version 1.2
+*@since 05.10.2018 
+*/
+package ru.job4j.calculate
