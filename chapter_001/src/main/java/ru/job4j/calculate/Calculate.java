4afa06ef30935b3879a6bb594db9ce8690f07f72
Volodymyrx <volodiax@mail.ru>
2018-10-05 21:38:57 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -3,15 +3,15 @@ package ru.job4j.calculate;
 *Calculate
 *
 *@author Volodymyr Martynenko (VolodymyrV.Martynenko@gmail.com)
-*project job4j lesson 1.1 
-*version 0.2
+*project job4j lesson 1.3 
+*@since 05.10.2018 
+*@version 1.2
 */
-
 public class Calculate
 {
 	/**
 	*main
-	*@param -no
+	*@param args -args, can be nothing
 	*print on console text 'Hello world'
 	*/
 	public static void main(String[] args)
