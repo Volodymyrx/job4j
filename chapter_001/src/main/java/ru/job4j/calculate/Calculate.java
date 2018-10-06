39247b5144eba57d4c565138009a58241107ccfe
Volodymyrx <volodiax@mail.ru>
2018-10-06 10:02:27 +0300

    1.4. JUnit [#4122]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -3,19 +3,28 @@ package ru.job4j.calculate;
 *Calculate
 *
 *@author Volodymyr Martynenko (VolodymyrV.Martynenko@gmail.com)
-*project job4j lesson 1.3 
-*@since 05.10.2018 
-*@version 1.2
+*project job4j lesson 1.4
+*@since 06.10.2018 
+*@version 1.4
 */
 public class Calculate
 {
 	/**
 	*main
 	*@param args -args, can be nothing
-	*print on console text 'Hello world'
+	*@return - nothing, print on console text 'Hello world'
 	*/
 	public static void main(String[] args)
 	{
-		System.out.println("Hello world");
+		System.out.println("Hello World");
 	}
+	
+	/**
+    * Method echo.
+    * @param name Your name.
+    * @return Echo plus your name.
+    */
+    public String echo(String name) {
+         return "Echo, echo, echo : " + name;
+    }
 }
---
+++ chapter_001/src/test/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,9 @@
+/**
+*Package for test class calculate.
+*
+*@author Volodymyr Martynenko (VolodymyrV.Martynenko@gmail.com)
+*project job4j lesson 1.4 
+*@version 1.0
+*@since 06.10.2018 
+*/
+package ru.job4j.calculate;
