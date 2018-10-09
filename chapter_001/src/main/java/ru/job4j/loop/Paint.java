ba8029052c2bc08ad7e7ec539daada20ba179402
Volodymyrx <volodiax@mail.ru>
2018-10-09 21:04:10 +0300

    5.4. Построить пирамиду в псевдографике. [#4412]-вернул назад треугольники для 5.5

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -11,6 +11,58 @@ package ru.job4j.loop;
  */
 public class Paint {
     /**
+     * method rightTrl paint right part of piramid
+     *
+     * @param height height of piramid
+     * @return string with image piramid
+     */
+    public String rightTrl(int height) {
+        // Буфер для результата.
+        StringBuilder screen = new StringBuilder();
+        // ширина будет равна высоте.
+        int weight = height;
+        // внешний цикл двигается по строкам.
+        for (int row = 0; row != height; row++) {
+            // внутренний цикл определяет положение ячейки в строке.
+            for (int column = 0; column != weight; column++) {
+                // если строка равна ячейки, то рисуем галку.
+                // в данном случае строка определяем, сколько галок будет на строке
+                if (row >= column) {
+                    screen.append("^");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            // добавляем перевод строки.
+            screen.append(System.lineSeparator());
+        }
+        // Получаем результат.
+        return screen.toString();
+    }
+
+    /**
+     * method leftTrl paint left part of piramid
+     *
+     * @param height height of piramid
+     * @return string with image piramid
+     */
+    public String leftTrl(int height) {
+        StringBuilder screen = new StringBuilder();
+        int weight = height;
+        for (int row = 0; row != height; row++) {
+            for (int column = 0; column != weight; column++) {
+                if (row >= weight - column - 1) {
+                    screen.append("^");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(System.lineSeparator());
+        }
+        return screen.toString();
+    }
+
+    /**
      * method paint piramid
      *
      * @param height height of piramid
