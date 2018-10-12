fbb79c7564b8171722c87539598d45b593018963
Volodymyrx <volodiax@mail.ru>
2018-10-12 22:11:01 +0300

    Контрольные вопросы часть 1. Базовый синтаксис.[#75242] - исправил

--- chapter_001/src/main/java/ru/job4j/array/ThirdArraySorted.java
+++ chapter_001/src/main/java/ru/job4j/array/ThirdArraySorted.java
@@ -6,45 +6,36 @@ package ru.job4j.array;
  *
  * @author Volodymyr Martynenko (VolodymyrV.Martynenko@gmail.com)
  * project job4j lesson question add
- * @version 1.0
- * @since 10.10.2018
+ * @version 1.1
+ * @since 12.10.2018
  */
 public class ThirdArraySorted {
     /**
      * method with two sorted array make third sorted array
-     * version 1
+     * version 2
      *
      * @param firstArray and secondArray are sorted int array
      * @return sorted array
      */
     public int[] makeThirdArraySorted(int[] firstArray, int[] secondArray) {
-        int[] thirdArray = new int[(firstArray.length + secondArray.length)];
-        int counterFirstArray = 0;
-        int currentNumber1;
-        int counterSecondArray = 0;
-        int currentNumber2;
-        for (int counterThirdArray = 0; counterThirdArray < thirdArray.length; counterThirdArray++) {
+        int[] thirdArray = new int[firstArray.length + secondArray.length];
+        int countFirst = 0;
+        int countSecond = 0;
+        int countThird = 0;
 
-            if (counterFirstArray >= (firstArray.length)) {
-                currentNumber1 = Integer.MAX_VALUE;
-            } else {
-                currentNumber1 = firstArray[counterFirstArray];
+        while (countThird < thirdArray.length) {
+            thirdArray[countThird] = firstArray[countFirst] < secondArray[countSecond] ? firstArray[countFirst++] : secondArray[countSecond++];
+            if (countFirst == firstArray.length) {
+                System.arraycopy(secondArray, countSecond, thirdArray, ++countThird, secondArray.length - countSecond);
+                break;
             }
-
-            if (counterSecondArray >= (secondArray.length)) {
-                currentNumber2 = Integer.MAX_VALUE;
-            } else {
-                currentNumber2 = secondArray[counterSecondArray];
+            if (countSecond == secondArray.length) {
+                System.arraycopy(firstArray, countFirst, thirdArray, ++countThird, firstArray.length - countFirst);
+                break;
             }
-
-            if (currentNumber1 <= currentNumber2) {
-                thirdArray[counterThirdArray] = currentNumber1;
-                counterFirstArray++;
-            } else {
-                thirdArray[counterThirdArray] = currentNumber2;
-                counterSecondArray++;
-            }
+            countThird++;
         }
         return thirdArray;
+
     }
 }
