368741a2fc507ce9a66c4a2d56d11dfa26852e3f
Volodymyrx <volodiax@mail.ru>
2018-10-13 20:22:04 +0300

    Контрольные вопросы часть 1. Базовый синтаксис.[#75242]- улучшил

--- chapter_001/src/main/java/ru/job4j/array/ThirdArraySorted.java
+++ chapter_001/src/main/java/ru/job4j/array/ThirdArraySorted.java
@@ -1,13 +1,15 @@
 package ru.job4j.array;
 
+import java.util.Arrays;
+
 /**
  * ThirdArraySorted
  * with two sorted array make third sorted array
  *
  * @author Volodymyr Martynenko (VolodymyrV.Martynenko@gmail.com)
  * project job4j lesson question add
- * @version 1.1
- * @since 12.10.2018
+ * @version 1.2
+ * @since 13.10.2018
  */
 public class ThirdArraySorted {
     /**
@@ -19,12 +21,18 @@ public class ThirdArraySorted {
      */
     public int[] makeThirdArraySorted(int[] firstArray, int[] secondArray) {
         int[] thirdArray = new int[firstArray.length + secondArray.length];
+        if (firstArray.length == 0) {
+            return Arrays.copyOf(secondArray, secondArray.length);
+        }
+        if (secondArray.length == 0) {
+            return Arrays.copyOf(firstArray, firstArray.length);
+        }
         int countFirst = 0;
         int countSecond = 0;
         int countThird = 0;
 
         while (countThird < thirdArray.length) {
-            thirdArray[countThird] = firstArray[countFirst] < secondArray[countSecond] ? firstArray[countFirst++] : secondArray[countSecond++];
+            thirdArray[countThird] = firstArray[countFirst] <= secondArray[countSecond] ? firstArray[countFirst++] : secondArray[countSecond++];
             if (countFirst == firstArray.length) {
                 System.arraycopy(secondArray, countSecond, thirdArray, ++countThird, secondArray.length - countSecond);
                 break;
--- chapter_001/src/test/java/ru/job4j/array/ThirdArraySortedTest.java
+++ chapter_001/src/test/java/ru/job4j/array/ThirdArraySortedTest.java
@@ -10,17 +10,58 @@ import static org.junit.Assert.assertThat;
  *
  * @author Volodymyr Martynenko (VolodymyrV.Martynenko@gmail.com)
  * project job4j lesson question add
- * @version 1.0
- * @since 12.10.2018
+ * @version 1.2
+ * @since 13.10.2018
  */
 public class ThirdArraySortedTest {
 
     @Test
-    public void checkSummaryArray() {
+    public void checkIfSecondShorterThenFirst() {
         ThirdArraySorted thirdArraySorted = new ThirdArraySorted();
         int[] inputArray1 = new int[]{2, 5, 8, 9, 24, 378};
         int[] inputArray2 = new int[]{0, 7, 10, 22};
         int[] expetArray = new int[]{0, 2, 5, 7, 8, 9, 10, 22, 24, 378};
+        int[] resutlArray = thirdArraySorted.makeThirdArraySorted(inputArray1, inputArray2);
+        assertThat(resutlArray, is(expetArray));
+    }
+
+    //TODO  ещё 3 теста, когда первый короче второго, второй пустой и оба пустые.
+    @Test
+    public void checkIfFirstShorterThenSecondThen() {
+        ThirdArraySorted thirdArraySorted = new ThirdArraySorted();
+        int[] inputArray1 = new int[]{0, 7, 10, 22};
+        int[] inputArray2 = new int[]{2, 5, 8, 9, 24, 378};
+        int[] expetArray = new int[]{0, 2, 5, 7, 8, 9, 10, 22, 24, 378};
+        int[] resutlArray = thirdArraySorted.makeThirdArraySorted(inputArray1, inputArray2);
+        assertThat(resutlArray, is(expetArray));
+    }
+
+    @Test
+    public void checkIfFirstArrayLengthZiro() {
+        ThirdArraySorted thirdArraySorted = new ThirdArraySorted();
+        int[] inputArray1 = new int[]{};
+        int[] inputArray2 = new int[]{0, 7, 10, 22};
+        int[] expetArray = new int[]{0, 7, 10, 22};
+        int[] resutlArray = thirdArraySorted.makeThirdArraySorted(inputArray1, inputArray2);
+        assertThat(resutlArray, is(expetArray));
+    }
+
+    @Test
+    public void checkIfSecondArrayLengthZiro() {
+        ThirdArraySorted thirdArraySorted = new ThirdArraySorted();
+        int[] inputArray1 = new int[]{0, 7, 10, 22};
+        int[] inputArray2 = new int[]{};
+        int[] expetArray = new int[]{0, 7, 10, 22};
+        int[] resutlArray = thirdArraySorted.makeThirdArraySorted(inputArray1, inputArray2);
+        assertThat(resutlArray, is(expetArray));
+    }
+
+    @Test
+    public void checkIfBothAreLengthZiro() {
+        ThirdArraySorted thirdArraySorted = new ThirdArraySorted();
+        int[] inputArray1 = new int[]{};
+        int[] inputArray2 = new int[]{};
+        int[] expetArray = new int[]{};
         int[] resutlArray = thirdArraySorted.makeThirdArraySorted(inputArray1, inputArray2);
         assertThat(resutlArray, is(expetArray));
     }
