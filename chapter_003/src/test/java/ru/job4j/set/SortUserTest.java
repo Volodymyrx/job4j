78f081540eeab7c5b18e321e3edd0c1b23892e02
Volodymyrx <volodiax@mail.ru>
2018-11-26 20:03:33 +0200

    1. JaCoCo. Процент покрытие тестами. [#31726]

--- chapter_003/src/test/java/ru/job4j/set/SortUserTest.java
+++ chapter_003/src/test/java/ru/job4j/set/SortUserTest.java
@@ -29,13 +29,13 @@ public class SortUserTest {
         listUser.add(new User("Ben", 25));
         listUser.add(new User("Ben", 21));
         Set<User> setUser = sortUser.sort(listUser);
-        Iterator<User> it=setUser.iterator();
-        int age1=it.next().getAge();
-        int age2=it.next().getAge();
-        int age3=it.next().getAge();
-        int age4=it.next().getAge();
-        boolean result=(age1<=age2)&&(age2<=age3)&&(age3<=age4);
-        assertThat(result, is(true ));
+        Iterator<User> it = setUser.iterator();
+        int age1 = it.next().getAge();
+        int age2 = it.next().getAge();
+        int age3 = it.next().getAge();
+        int age4 = it.next().getAge();
+        boolean result = (age1 <= age2) && (age2 <= age3) && (age3 <= age4);
+        assertThat(result, is(true));
     }
 
 
@@ -48,11 +48,11 @@ public class SortUserTest {
         listUser.add(new User("Ben", 25));
         listUser.add(new User("Ben", 21));
         Set<User> setUser = sortUser.sort(listUser);
-        assertThat(setUser.toString(), is("" +
-                "[User{name='Abraham', age=20}, " +
-                "User{name='Ben', age=21}, " +
-                "User{name='Ivan', age=21}, " +
-                "User{name='Ben', age=25}]"
+        assertThat(setUser.toString(), is(""
+                + "[User{name='Abraham', age=20}, "
+                + "User{name='Ben', age=21}, "
+                + "User{name='Ivan', age=21}, "
+                + "User{name='Ben', age=25}]"
         ));
     }
 
@@ -67,11 +67,11 @@ public class SortUserTest {
         listUser.add(new User("Abraham", 20));
         listUser.add(new User("Li", 25));
         listUser.add(new User("Ben", 30));
-        assertThat(sortUser.sortNameLength(listUser).toString(), is("" +
-                "[User{name='Li', age=25}, " +
-                "User{name='Ben', age=30}, " +
-                "User{name='Ivan', age=21}, " +
-                "User{name='Abraham', age=20}]"
+        assertThat(sortUser.sortNameLength(listUser).toString(), is(""
+                + "[User{name='Li', age=25}, "
+                + "User{name='Ben', age=30}, "
+                + "User{name='Ivan', age=21}, "
+                + "User{name='Abraham', age=20}]"
         ));
     }
 
@@ -86,11 +86,11 @@ public class SortUserTest {
         listUser.add(new User("Abraham", 20));
         listUser.add(new User("Livenson", 25));
         listUser.add(new User("Ivan", 18));
-        assertThat(sortUser.sortByAllFields(listUser).toString(), is("" +
-                "[User{name='Abraham', age=20}, " +
-                "User{name='Ivan', age=18}, " +
-                "User{name='Ivan', age=21}, " +
-                "User{name='Livenson', age=25}]"
+        assertThat(sortUser.sortByAllFields(listUser).toString(), is(""
+                + "[User{name='Abraham', age=20}, "
+                + "User{name='Ivan', age=18}, "
+                + "User{name='Ivan', age=21}, "
+                + "User{name='Livenson', age=25}]"
         ));
     }
 }
