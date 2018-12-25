788b71a1e5d6aec01e09058204658291a56f867c
Volodymyrx <volodiax@mail.ru>
2018-12-25 20:47:38 +0200

    0. Функциональные итерфейсы.  [#58165]2

---
+++ chapter_004/src/main/java/ru/job4j/lambda/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Package for task-lambda
+ * class Calculator
+ * class MathUtil
+ * class UserConvert
+ *
+ * @author Volodymyr Martynenko (VolodymyrV.Martynenko@gmail.com)
+ * project job4j lessons 4.0
+ * @version 1.0
+ * @since 25.12.2018
+ */
+package ru.job4j.lambda;
---
+++ chapter_004/src/test/java/ru/job4j/lambda/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Package for task-lambda
+ * class CalculatorTest
+ * class MathUtilTest
+ * class
+ *
+ * @author Volodymyr Martynenko (VolodymyrV.Martynenko@gmail.com)
+ * project job4j lessons 4.0
+ * @version 1.0
+ * @since 25.12.2018
+ */
+package ru.job4j.lambda;
